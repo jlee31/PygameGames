3f2300ff716f68a03c09f3ee19ce0e98b674580e
Joseph Lee <hijosephl31@gmail.com>
2025-07-03 20:24:37 -0400

    Added player class with movement

---
+++ pydewvalley-clone/code/player.py
@@ -0,0 +1,50 @@
+import pygame
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, group):
+        super().__init__(group);
+
+
+        # Player Model
+
+        self.image = pygame.Surface((100, 200))
+        self.image.fill('green');
+        self.rect = self.image.get_rect(center = pos)
+
+        # Movement Attributes
+        self.direction = pygame.math.Vector2()
+        self.position = pygame.math.Vector2(self.rect.center)
+        self.speed = 200;
+    
+
+    def input(self):
+        keys = pygame.key.get_pressed();
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+    def move(self, dt):
+        if (self.direction.magnitude() > 0):
+            self.direction = self.direction.normalize()
+
+
+        self.position.x += self.direction.x * self.speed * dt
+        self.rect.x = self.position.x
+        self.position.y += self.direction.y * self.speed * dt
+        self.rect.y = self.position.y
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
--- pydewvalley-clone/code/settings.py
+++ pydewvalley-clone/code/settings.py
@@ -1,5 +1,6 @@
 from pygame.math import Vector2
-# screen
+
+# Screen Settings
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 TILE_SIZE = 64
