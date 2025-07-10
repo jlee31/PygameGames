f2ddd7dc7d33f57a5e6d85af749d2997fb2e2ab4
Joseph Lee <hijosephl31@gmail.com>
2025-07-09 22:39:04 -0400

    added overlay for selected seed

---
+++ pydewvalley-clone/code/overlay.py
@@ -0,0 +1,26 @@
+import pygame
+from settings import *
+
+class Overlay:
+    def __init__(self, player):
+        
+        # General Setup
+        self.displaySurface = pygame.display.get_surface()
+        self.player = player
+
+        # Importing 
+        overlayPath = '../graphics/overlay/'
+        self.tool_surfaces = {tool: pygame.image.load(f'{overlayPath}{tool}.png').convert_alpha() for tool in player.tools}
+        self.seed_surfaces = {seed: pygame.image.load(f'{overlayPath}{seed}.png').convert_alpha() for seed in player.seeds}
+
+    def display(self):
+
+        # show tools
+        toolSurf = self.tool_surfaces[self.player.selectedTool]
+        toolRect = toolSurf.get_rect(midbottom=(OVERLAY_POSITIONS['tool']))
+        self.displaySurface.blit(toolSurf, toolRect)
+
+        # show seeds
+        seedSurf = self.seed_surfaces[self.player.selectedSeed]
+        seedRect = toolSurf.get_rect(midbottom=(OVERLAY_POSITIONS['seed']))
+        self.displaySurface.blit(seedSurf, seedRect)
